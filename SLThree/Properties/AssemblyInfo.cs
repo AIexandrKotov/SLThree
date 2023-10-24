3ac77f4f8211d97064eb804e391328b47befd4ad
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-10-24 09:02:54 +0300

    match statement
    fix #2

--- SLThree/Properties/AssemblyInfo.cs
+++ SLThree/Properties/AssemblyInfo.cs
@@ -43,8 +43,8 @@ public static class SLTVersion {
     public const string Major = "0"; //vh
     public const string Minor = "1"; //vh
     public const string Build = "1"; //vh
-    public const string Revision = "168"; //vh
-    public const long LastUpdate = 638336881507973753; //vh
+    public const string Revision = "219"; //vh
+    public const long LastUpdate = 638337241280072766; //vh
 
     public const string Version = Major + "." + Minor + "." + Build + "." + Revision;
     public const string VersionWithoutRevision = Major + "." + Minor + "." + Build;
