34e1a71b6babd61186cde6d570308de8d5977ce0
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-10-24 13:28:35 +0300

    SLT separate versioning

---
+++ SLThree/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLThree
+{
+    public partial class Parser
+    {
+        public BaseStatement ParseScript(string s, string filename = null) => Parse("#SLT# " + s, filename);
+    }
+}
