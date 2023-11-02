9b4be8839e1f7852c13526a140e1f21df848d58d
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-11-02 21:10:01 +0300

    fix #28

--- SLThree/Properties/AssemblyInfo.cs
+++ SLThree/Properties/AssemblyInfo.cs
@@ -43,8 +43,8 @@ public static class SLTVersion {
     public const string Major = "0"; //vh
     public const string Minor = "2"; //vh
     public const string Build = "0"; //vh
-    public const string Revision = "762"; //vh
-    public const long LastUpdate = 638345449084390873; //vh
+    public const string Revision = "764"; //vh
+    public const long LastUpdate = 638345453362770753; //vh
 
     public const string Version = Major + "." + Minor + "." + Build + "." + Revision;
     public const string VersionWithoutRevision = Major + "." + Minor + "." + Build;
