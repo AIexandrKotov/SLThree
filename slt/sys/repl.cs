c3d3b1ab2db3d8fd2df4c8406f9bdefd3eab3f78
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-11-15 00:34:45 +0300

    slt.exe assembly for types
    specified amount of output list, tuples, dicts
    running files with repl.run_file

--- slt/sys/repl.cs
+++ slt/sys/repl.cs
@@ -14,6 +14,17 @@ namespace slt.sys
         public static void out_locals(ExecutionContext.ContextWrap context) => Program.OutLocals(context.pred, -2);
         public static void out_locals_typed(ExecutionContext.ContextWrap context) => Program.OutLocals(context.pred, -2, true);
         public static void new_context(ExecutionContext.ContextWrap context) => Program.REPLContext = context.pred;
+        public static void command(string str) => Program.REPLCommand(">" + str);
+
+        public static int count = 20;
+        public static ExecutionContext.ContextWrap run_file(string file) 
+            => new ExecutionContext.ContextWrap(Program.InvokeFile(file, Program.GetNewREPLContext(), Encoding.UTF8));
+        public static ExecutionContext.ContextWrap run_file(string file, Encoding encoding) 
+            => new ExecutionContext.ContextWrap(Program.InvokeFile(file, Program.GetNewREPLContext(), encoding));
+        public static ExecutionContext.ContextWrap run_file_in(string file, ExecutionContext.ContextWrap context) 
+            => new ExecutionContext.ContextWrap(Program.InvokeFile(file, context.pred, Encoding.UTF8));
+        public static ExecutionContext.ContextWrap run_file_in(string file, Encoding encoding, ExecutionContext.ContextWrap context) 
+            => new ExecutionContext.ContextWrap(Program.InvokeFile(file, context.pred, encoding));
     }
 #pragma warning restore IDE1006 // Стили именования
 }
