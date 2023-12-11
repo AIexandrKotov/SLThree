5b4600be400d3048274f72338fba8e468a2b53ee
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-12-11 20:58:37 +0300

    lang 0.6.0 supporting

---
+++ slt/compatibility/LANG_060.cs
@@ -0,0 +1,18 @@
+﻿using SLThree.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace slt
+{
+    public static class LANG_060
+    {
+        public static bool Supports { get; internal set; }
+        private static FieldInfo TypeofExpression_RegistredAssemblies;
+        internal static void Init()
+        {
+            TypeofExpression_RegistredAssemblies = Program.SLThreeAssembly.GetType("SLThree.TypeofExpression").GetField("RegistredAssemblies");
+            TypeofExpression_RegistredAssemblies.GetValue(null).Cast<List<Assembly>>().Add(typeof(LANG_060).Assembly);
+        }
+    }
+}
