d56ca99a837000f148a0d18488fc44da8cf86d14
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-01-11 18:14:11 +0300

    null coalescing

---
+++ SLThree/Expressions/NullCoalescingExpression.cs
@@ -0,0 +1,28 @@
+﻿using SLThree.Extensions.Cloning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLThree
+{
+    internal class NullCoalescingExpression : ExpressionBinary
+    {
+        public override string Operator => "??";
+        public NullCoalescingExpression(BaseExpression left, BaseExpression right, SourceContext context, bool priority = false) : base(left, right, context, priority) { }
+        public NullCoalescingExpression() : base() { }
+
+        public override object GetValue(ExecutionContext context)
+        {
+            var left = Left.GetValue(context);
+            if (left == null) return Right.GetValue(context);
+            return left;
+        }
+
+        public override object Clone()
+        {
+            return new NullCoalescingExpression(Left.CloneCast(), Right.CloneCast(), SourceContext.CloneCast(), PrioriryRaised);
+        }
+    }
+}
--- SLThree/Properties/AssemblyInfo.cs
+++ SLThree/Properties/AssemblyInfo.cs
@@ -69,8 +69,8 @@ public static class SLTVersion {
     public const string Major = "0"; //vh
     public const string Minor = "6"; //vh
     public const string Build = "0"; //vh
-    public const string Revision = "1869"; //vh
-    public const long LastUpdate = 638405813031527396; //vh
+    public const string Revision = "1872"; //vh
+    public const long LastUpdate = 638405821499483145; //vh
 
     public const string Version = Major + "." + Minor + "." + Build + "." + Revision;
     public const string VersionWithoutRevision = Major + "." + Minor + "." + Build;
@@ -104,7 +104,7 @@ public static class SLTVersion {
         }
     }
 
-    public static string Edition { get; } = "Rybinsk Update";
+    public static string Edition { get; } = "Surgut Update";
 
     public static string GetTitle()
     {
