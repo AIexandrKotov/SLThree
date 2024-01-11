46bc770334701dca7a662f473145cc9cfc9061e1
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-01-11 19:37:02 +0300

    single statements

--- SLThree/Properties/AssemblyInfo.cs
+++ SLThree/Properties/AssemblyInfo.cs
@@ -69,8 +69,8 @@ public static class SLTVersion {
     public const string Major = "0"; //vh
     public const string Minor = "6"; //vh
     public const string Build = "0"; //vh
-    public const string Revision = "1872"; //vh
-    public const long LastUpdate = 638405821499483145; //vh
+    public const string Revision = "1898"; //vh
+    public const long LastUpdate = 638405852836628235; //vh
 
     public const string Version = Major + "." + Minor + "." + Build + "." + Revision;
     public const string VersionWithoutRevision = Major + "." + Minor + "." + Build;
