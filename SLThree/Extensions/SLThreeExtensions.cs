a8b83ac34c4a2767d4025e0e4aea2d9109fe629e
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-04-21 13:44:51 +0300

    fix #96

--- SLThree/Extensions/SLThreeExtensions.cs
+++ SLThree/Extensions/SLThreeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SLThree.Extensions
 {
@@ -14,6 +15,21 @@ namespace SLThree.Extensions
             if (precision < 2) precision = 2;
 
             return d.ToString($"P{precision - 2}");
+        }
+
+        public static string ReadString(this Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+        public static string[] ReadStrings(this Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return sr.ReadToEnd().Split(new string[1] { Environment.NewLine }, StringSplitOptions.None);
+            }
         }
     }
 }
