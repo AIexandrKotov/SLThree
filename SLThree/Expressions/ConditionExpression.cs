fcd7d955450fd4ab5f7c0b4edb21867cd3fe275e
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-06-09 17:15:39 +0300

    condition expression

---
+++ SLThree/Expressions/ConditionExpression.cs
@@ -0,0 +1,56 @@
+﻿using SLThree.Extensions.Cloning;
+using System.Linq;
+
+namespace SLThree
+{
+    public class ConditionExpression : BaseExpression
+    {
+        public BaseExpression Condition { get; set; }
+        public BaseStatement[] Body { get; set; }
+
+        public ConditionExpression() { }
+        public ConditionExpression(BaseExpression condition, BaseStatement[] body, int falsestart, SourceContext context) : base(context)
+        {
+            Condition = condition;
+            Body = body;
+            count = Body.Length;
+            this.falsestart = falsestart;
+        }
+
+        public ConditionExpression(BaseExpression condition, StatementList trueBlock, StatementList falseBlock, SourceContext context) : base(context)
+        {
+            Condition = condition;
+            count = trueBlock.Statements.Length + falseBlock.Statements.Length;
+            Body = new BaseStatement[count];
+            trueBlock.Statements.CopyTo(Body, 0);
+            falsestart = trueBlock.Statements.Length;
+            falseBlock.Statements.CopyTo(Body, falsestart);
+        }
+        private int count;
+        private int falsestart;
+
+        public BaseStatement[] IfBody => Body.Take(falsestart).ToArray();
+        public BaseStatement[] ElseBody => Body.Skip(falsestart).ToArray();
+
+        public override string ExpressionToString() => $"if ({Condition}) {{{Body}}}";
+
+        public override object GetValue(ExecutionContext context)
+        {
+            var ret = default(object);
+            var cond = (bool)Condition.GetValue(context);
+            var start = cond ? 0 : falsestart;
+            var end = cond ? falsestart : count;
+            for (var i = start; i < end; i++)
+            {
+                ret = Body[i].GetValue(context);
+                if (context.Returned || context.Broken || context.Continued) break;
+            }
+            return ret;
+        }
+
+        public override object Clone()
+        {
+            return new ConditionExpression(Condition.CloneCast(), Body.CloneArray(), falsestart, SourceContext.CloneCast());
+        }
+    }
+}
