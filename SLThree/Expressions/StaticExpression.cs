c976ff843f18f4a197557cf52fecba88df178e30
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-06-10 01:56:24 +0300

    static expression

---
+++ SLThree/Expressions/StaticExpression.cs
@@ -0,0 +1,31 @@
+﻿using SLThree.Extensions.Cloning;
+
+namespace SLThree
+{
+    public class StaticExpression : BaseExpression
+    {
+        public BaseExpression Right;
+
+        public StaticExpression(BaseExpression right, SourceContext context) : base(context)
+        {
+            Right = right;
+        }
+
+        public override string ExpressionToString() => $"static {Right}";
+
+        private bool done;
+        private object obj;
+
+        public override object GetValue(ExecutionContext context)
+        {
+            if (done) return obj;
+            done = true;
+            return obj = Right.GetValue(context);
+        }
+
+        public override object Clone()
+        {
+            return new StaticExpression(Right.CloneCast(), SourceContext.CloneCast());
+        }
+    }
+}
