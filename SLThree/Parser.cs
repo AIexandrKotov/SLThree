6030e6a73b7ef7f89fcfee18e22e253cbd140ec3
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-06-19 18:24:07 +0300

    static reorder

--- SLThree/Parser.cs
+++ SLThree/Parser.cs
@@ -1,4 +1,5 @@
 ﻿using SLThree.Extensions;
+using SLThree.Extensions.Cloning;
 using SLThree.Visitors;
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,22 @@ namespace SLThree
     {
         public static readonly Parser This = new Parser();
 
-        public BaseExpression ParseExpression(string s) => this.Parse("#EXPR# " + s).Cast<ExpressionStatement>().Expression;
+        public BaseExpression ParseExpression(string s)
+        {
+            var ret = this.Parse("#EXPR# " + s).Cast<ExpressionStatement>().Expression;
+            return ret;
+        }
         public object EvalExpression(string s, ExecutionContext context = null)
         {
             if (context == null) context = new ExecutionContext();
             return ParseExpression(s).GetValue(context);
         }
 
-        public BaseStatement ParseScript(string s, string filename = null) => this.Parse("#SLT# " + s, filename);
+        public BaseStatement ParseScript(string s, string filename = null)
+        {
+            var ret = this.Parse("#SLT# " + s, filename);
+            return ret;
+        }
         public ExecutionContext RunScript(string s, string filename = null, ExecutionContext context = null)
         {
             var parsed = ParseScript(s, filename);
@@ -104,17 +113,97 @@ namespace SLThree
             return right;
         }
 
-        private static BaseExpression ReorderStatic(StaticExpression expression)
+        private sealed class VFirst<T>: AbstractVisitor where T: class, ExecutionContext.IExecutable
         {
-            var ret = expression;
-            if (ret.Right is BinaryAssign)
-            {
-                var left = ret.Right;
-                while (left is BinaryAssign assign)
-                    left = assign.Right;
+            public T Found = null;
+            public Func<T, bool> Predicate = x => true;
 
+            public override void VisitExpression(BaseExpression expression)
+            {
+                if (expression is T t && Predicate(t))
+                {
+                    Found = t;
+                    return;
+                }
+                base.VisitExpression(expression);
             }
-            throw new NotImplementedException();
+            public override void VisitStatement(BaseStatement statement)
+            {
+                if (statement is T t && Predicate(t))
+                {
+                    Found = t;
+                    return;
+                }
+                base.VisitStatement(statement);
+            }
+        }
+        private static bool Any<T>(object o) where T : class, ExecutionContext.IExecutable
+        {
+            var has = new VFirst<T>();
+            has.VisitAny(o);
+            return has.Found != null;
+        }
+        private static bool Any<T>(object o, out T ret) where T : class, ExecutionContext.IExecutable
+        {
+            var has = new VFirst<T>();
+            has.VisitAny(o);
+            ret = has.Found;
+            return ret != null;
+        }
+        private static bool Any<T>(object o, Func<T, bool> predicate) where T : class, ExecutionContext.IExecutable
+        {
+            var has = new VFirst<T>();
+            has.Predicate = predicate;
+            has.VisitAny(o);
+            return has.Found != null;
+        }
+        private static bool Any<T>(object o, Func<T, bool> predicate, out T ret) where T : class, ExecutionContext.IExecutable
+        {
+            var has = new VFirst<T>();
+            has.Predicate = predicate;
+            has.VisitAny(o);
+            ret = has.Found;
+            return has.Found != null;
+        }
+
+        public static void UncachebleCheck(object o, string suffix = " after static")
+        {
+            if (Any<BinaryAssign>(o, out var wrongassign))
+                throw new LogicalError($"Unexpected assign {wrongassign}{suffix}", wrongassign.SourceContext);
+            if (Any<FunctionDefinition>(o, func => func.FunctionName != null, out var wrongmethod))
+                throw new LogicalError($"Unexpected named method{suffix}", wrongmethod.SourceContext);
+            if (Any<CreatorContext>(o, context => context.Name != null, out var wrongcontext))
+                throw new LogicalError($"Unexpected named context{suffix}", wrongassign.SourceContext);
+            if (Any<CreatorInstance>(o, instance => instance.Name != null, out var wronginstance))
+                throw new LogicalError($"Unexpected named instance{suffix}", wronginstance.SourceContext);
+        }
+        private BaseExpression ReorderStatic(StaticExpression expression)
+        {
+            //if (did.TryGetValue(expression, out var expr)) return expr;
+            if (expression.Right is BinaryAssign)
+            {
+                var input = expression.CloneCast();
+                var left = input.Right as BinaryAssign;
+                while (left is BinaryAssign assign && assign.Right is BinaryAssign)
+                    left = assign.Right as BinaryAssign;
+                UncachebleCheck(left.Right);
+                var ret = input.Right;
+                input.Right = left.Right;
+                left.Right = input;
+                return ret;
+            }
+            if (expression.Right is CreatorContext context && context.HasName)
+                return new BinaryAssign(context.Name.CloneCast(), expression, context.SourceContext);
+            if (expression.Right is CreatorInstance instance && instance.Name != null)
+                return new BinaryAssign(instance.Name.CloneCast(), expression, instance.SourceContext);
+            return ReorderStaticMethod(expression);
+        }
+        private BaseExpression ReorderStaticMethod(StaticExpression expression)
+        {
+            if (expression.Right is FunctionDefinition func && func.FunctionName != null)
+                return new BinaryAssign(func.FunctionName.CloneCast(), expression, expression.Right.SourceContext);
+            UncachebleCheck(expression.Right);
+            return expression;
         }
 
         private static T Panic<T>(SLTException exception)
