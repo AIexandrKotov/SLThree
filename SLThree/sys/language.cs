b7f48ef5a9b062b32a9b5b1f8a15239e9eb909ec
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-07-31 16:18:25 +0300

    language.show() refactoring

--- SLThree/sys/language.cs
+++ SLThree/sys/language.cs
@@ -1,8 +1,10 @@
-﻿using SLThree.Native;
+﻿using SLThree.Extensions;
+using SLThree.Native;
 using SLThree.Visitors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,66 +24,102 @@ namespace SLThree.sys
                 type = type.BaseType;
             }
         }
-
-        public static void show()
+        public static IEnumerable<Type> GetAncestorsInclude(Type type)
         {
-            var types = SLThree.GetTypes();
-            var statements = types.Where(x => GetAncestors(x).Contains(typeof(BaseStatement)));
-            var expressions = types.Where(x => GetAncestors(x).Contains(typeof(BaseExpression)));
-            expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(Literal)));
-            expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(Special)));
-            expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(BinaryOperator)));
-            expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(UnaryOperator)));
+            yield return type;
+            type = type.BaseType;
+            while (type != null)
+            {
+                yield return type;
+                type = type.BaseType;
+            }
+        }
 
-            //name, AV define, TV define, XMLV define
-            var infos = new List<(string, bool, bool, bool, bool, bool)>();
-            foreach (var statement in statements)
+        public static class VisitorInfoBuilder
+        {
+
+            public static IEnumerable<(string, bool[])> ResearchTypes(Type interface_type, string visit_method_name, IEnumerable<Type> target_types, IEnumerable<Type> visitors)
             {
-                var AVdefine = typeof(IStatementVisitor).GetMethods().Any(x => x.Name == "VisitStatement" && x.GetParameters()[0].ParameterType == statement);
-                var TVdefine = typeof(TreeViewer).GetMethods().Any(x => x.Name == "VisitStatement" && x.GetParameters()[0].ParameterType == statement && x.DeclaringType != typeof(AbstractVisitor));
-                var XMLdefine = typeof(XmlViewer).GetMethods().Any(x => x.Name == "VisitStatement" && x.GetParameters()[0].ParameterType == statement && x.DeclaringType != typeof(AbstractVisitor));
-                var NETdefine = typeof(NETGenerator).GetMethods().Any(x => x.Name == "VisitStatement" && x.GetParameters()[0].ParameterType == statement && x.DeclaringType != typeof(AbstractVisitor));
-                infos.Add((statement.Name, false, AVdefine, TVdefine, XMLdefine, NETdefine));
+                foreach (var target in target_types)
+                {
+                    var AVdefine = interface_type.GetMethods().Any(x => x.Name == visit_method_name && x.GetParameters()[0].ParameterType == target);
+                    yield return (target.Name, Enumerable.Concat(Enumerable.Repeat(AVdefine, 1), visitors.Select(t => t.GetMethods().Any(x => x.Name == visit_method_name && x.GetParameters()[0].ParameterType == target && x.DeclaringType != typeof(AbstractVisitor)))).ToArray());
+                }
             }
-            foreach (var expression in expressions)
+
+            static VisitorInfoBuilder()
             {
-                var AVdefine = typeof(IExpressionVisitor).GetMethods().Any(x => x.Name == "VisitExpression" && x.GetParameters()[0].ParameterType == expression);
-                var TVdefine = typeof(TreeViewer).GetMethods().Any(x => x.Name == "VisitExpression" && x.GetParameters()[0].ParameterType == expression && x.DeclaringType != typeof(AbstractVisitor));
-                var XMLdefine = typeof(XmlViewer).GetMethods().Any(x => x.Name == "VisitExpression" && x.GetParameters()[0].ParameterType == expression && x.DeclaringType != typeof(AbstractVisitor));
-                var NETdefine = typeof(NETGenerator).GetMethods().Any(x => x.Name == "VisitExpression" && x.GetParameters()[0].ParameterType == expression && x.DeclaringType != typeof(AbstractVisitor));
-                infos.Add((expression.Name, true, AVdefine, TVdefine, XMLdefine, NETdefine));
+                (Statements, Expressions, Constraints) = GetTargetTypes();
             }
+
+            public static readonly Type[] Statements, Expressions, Constraints;
+
+            public static (Type[], Type[], Type[]) GetTargetTypes()
+            {
+                var types = SLThree.GetTypes();
+                var statements = types.Where(x => GetAncestors(x).Contains(typeof(BaseStatement)));
+                var expressions = types.Where(x => GetAncestors(x).Contains(typeof(BaseExpression)));
+                var constraints = types.Where(x => GetAncestorsInclude(x).Contains(typeof(TemplateMethod.ConstraintDefinition)));
+                expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(Literal)));
+                expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(Special)));
+                expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(BinaryOperator)));
+                expressions = expressions.Where(x => !GetAncestors(x).Contains(typeof(UnaryOperator)));
+                expressions = expressions.Where(x => !GetAncestorsInclude(x).Contains(typeof(TemplateMethod.ConstraintDefinition)));
+
+                return (statements.ToArray(), expressions.ToArray(), constraints.ToArray());
+            }
+
+            public static IEnumerable<(string, ConsoleColor, bool[])> ResearchAllTypes(IEnumerable<Type> visitors)
+            {
+                foreach (var x in ResearchTypes(typeof(IStatementVisitor), "VisitStatement", Statements, visitors))
+                    yield return (x.Item1, ConsoleColor.Magenta, x.Item2);
+                foreach (var x in ResearchTypes(typeof(IExpressionVisitor), "VisitExpression", Expressions, visitors))
+                    yield return (x.Item1, ConsoleColor.Cyan, x.Item2);
+                foreach (var x in ResearchTypes(typeof(IConstraintVisitor), "VisitConstraint", Constraints, visitors))
+                    yield return (x.Item1, ConsoleColor.Yellow, x.Item2);
+            }
+        }
+
+        public static void show(params Type[] targets)
+        {
+            var columnnames = targets.Select(x => x.Name.Where(c => char.IsUpper(c)).JoinIntoString("") + "def").ToList();
+            columnnames.Insert(0, "Element name");
+            columnnames.Insert(1, "AVdef");
+
+            columnnames = columnnames.ConvertAll(x => x.PadRight(Math.Max(7, x.Length)));
+
+            var infos = VisitorInfoBuilder.ResearchAllTypes(targets).ToList();
             infos.Sort((x, y) => (string.Compare(x.Item1, y.Item1)));
-            var nameident = infos.Max(x => x.Item1.Length);
+            var max = infos.Max(x => x.Item1.Length);
+            columnnames[0] = columnnames[0].PadRight(max);
+
             var defs = new Dictionary<bool, string> { { false, "not" }, { true, "defined" } };
             var colors = new Dictionary<bool, ConsoleColor> { { false, ConsoleColor.Red }, { true, ConsoleColor.Green } };
             Console.ResetColor();
-            Console.WriteLine($"{"Element name".PadRight(nameident)} │  AVdef  │  TVdef  │  XMLdef │ NETdef  ");
+
+
+            Console.WriteLine(columnnames.JoinIntoString(" │ "));
             foreach (var x in infos)
             {
-                Console.ForegroundColor = x.Item2 ? ConsoleColor.Cyan : ConsoleColor.Magenta;
-                Console.Write(x.Item1.PadRight(nameident));
+                Console.ForegroundColor = x.Item2;
+                Console.Write(x.Item1.PadRight(columnnames[0].Length));
                 Console.ResetColor();
-                Console.Write(" │ ");
-                Console.ForegroundColor = colors[x.Item3];
-                Console.Write(defs[x.Item3].PadRight(7));
-                Console.ResetColor();
-                Console.Write(" │ ");
-                Console.ForegroundColor = colors[x.Item4];
-                Console.Write(defs[x.Item4].PadRight(7));
-                Console.ResetColor();
-                Console.Write(" │ ");
-                Console.ForegroundColor = colors[x.Item5];
-                Console.Write(defs[x.Item5].PadRight(7));
-                Console.ResetColor();
-                Console.Write(" │ ");
-                Console.ForegroundColor = colors[x.Item6];
-                Console.Write(defs[x.Item6].PadRight(7));
-                Console.ResetColor();
+                for (var i = 0; i < columnnames.Count - 1; i++)
+                {
+                    Console.Write(" │ ");
+                    Console.ForegroundColor = colors[x.Item3[i]];
+                    Console.Write(defs[x.Item3[i]].PadRight(7));
+                    Console.ResetColor();
+                }
                 Console.WriteLine();
             }
 
             Console.WriteLine();
+        }
+
+        public static void show()
+        {
+            show(typeof(TreeViewer), typeof(XmlViewer), typeof(NETGenerator));
         }
     }
 #pragma warning restore IDE1006 // Стили именования
