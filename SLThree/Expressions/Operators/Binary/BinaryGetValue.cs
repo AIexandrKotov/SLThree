e7ff44db385caec1933e3c7c8bc1b370394e1fe4
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-08-05 09:22:13 +0300

    invokator operator

---
+++ SLThree/Expressions/Operators/Binary/BinaryGetValue.cs
@@ -0,0 +1,39 @@
+﻿using SLThree.Extensions;
+using SLThree.Extensions.Cloning;
+
+namespace SLThree
+{
+    public class BinaryGetValue : BinaryOperator
+    {
+        public BinaryGetValue(BaseExpression left, BaseExpression right, SourceContext context, bool priorityRaised = false) : base(left, right, context, priorityRaised)
+        {
+
+        }
+
+        public override object GetValue(ExecutionContext context)
+        {
+            //return Right.GetValue(context).Cast<ExecutionContext.IExecutable>().GetValue(((ContextWrap)Left.GetValue(context)).Context);
+            return Right.GetValue(((ContextWrap)Left.GetValue(context)).Context);
+        }
+
+        public override object Clone() => new BinaryGetValue(Left.CloneCast(), Right.CloneCast(), SourceContext.CloneCast(), PrioriryRaised);
+
+        public override string Operator => "->";
+    }
+    public class BinaryGetRuntimeValue : BinaryOperator
+    {
+        public BinaryGetRuntimeValue(BaseExpression left, BaseExpression right, SourceContext context, bool priorityRaised = false) : base(left, right, context, priorityRaised)
+        {
+
+        }
+
+        public override object GetValue(ExecutionContext context)
+        {
+            return Right.GetValue(context).Cast<ExecutionContext.IExecutable>().GetValue(((ContextWrap)Left.GetValue(context)).Context);
+        }
+
+        public override object Clone() => new BinaryGetRuntimeValue(Left.CloneCast(), Right.CloneCast(), SourceContext.CloneCast(), PrioriryRaised);
+
+        public override string Operator => "-->";
+    }
+}
