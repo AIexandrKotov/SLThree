c6a1af01dba0761b8e5ca1ac1a1bd4f2d74b3ee8
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-08-18 12:58:57 +0300

    including and excluding ranges

--- SLThree/Expressions/Creators/CreatorRange.cs
+++ SLThree/Expressions/Creators/CreatorRange.cs
@@ -12,13 +12,15 @@ namespace SLThree
         public BaseExpression LowerBound;
         public BaseExpression UpperBound;
         public TypenameExpression RangeType;
+        public bool Excluding;
 
-        public CreatorRange(BaseExpression lowerBound, BaseExpression upperBound, SourceContext context) : this(lowerBound, upperBound, null, context) { }
-        public CreatorRange(BaseExpression lowerBound, BaseExpression upperBound, TypenameExpression type, SourceContext context) : base(context)
+        public CreatorRange(BaseExpression lowerBound, BaseExpression upperBound, bool excluding, SourceContext context) : this(lowerBound, upperBound, null, excluding, context) { }
+        public CreatorRange(BaseExpression lowerBound, BaseExpression upperBound, TypenameExpression type, bool excluding, SourceContext context) : base(context)
         {
             LowerBound = lowerBound;
             UpperBound = upperBound;
             RangeType = type;
+            Excluding = excluding;
         }
 
         #region Ranges
@@ -27,10 +29,10 @@ namespace SLThree
             public long Lower;
             public long Upper;
 
-            public RangeEnumerator(long lower, long upper)
+            public RangeEnumerator(long lower, long upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = upper - (excluding ? 1 : 0);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -47,10 +49,10 @@ namespace SLThree
             public long Lower;
             public long Upper;
 
-            public LongRangeEnumerator(long lower, long upper)
+            public LongRangeEnumerator(long lower, long upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = upper - (excluding ? 1 : 0);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -67,10 +69,10 @@ namespace SLThree
             public ulong Lower;
             public ulong Upper;
             
-            public ULongRangeEnumerator(ulong lower, ulong upper)
+            public ULongRangeEnumerator(ulong lower, ulong upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = upper - (excluding ? 1u : 0);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -87,10 +89,10 @@ namespace SLThree
             public int Lower;
             public int Upper;
 
-            public IntRangeEnumerator(int lower, int upper)
+            public IntRangeEnumerator(int lower, int upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = upper - (excluding ? 1 : 0);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -107,10 +109,10 @@ namespace SLThree
             public uint Lower;
             public uint Upper;
 
-            public UIntRangeEnumerator(uint lower, uint upper)
+            public UIntRangeEnumerator(uint lower, uint upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = upper - (excluding ? 1u : 0);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -127,10 +129,10 @@ namespace SLThree
             public short Lower;
             public short Upper;
 
-            public ShortRangeEnumerator(short lower, short upper)
+            public ShortRangeEnumerator(short lower, short upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = (short)(upper - (excluding ? 1 : 0));
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -147,10 +149,10 @@ namespace SLThree
             public ushort Lower;
             public ushort Upper;
 
-            public UShortRangeEnumerator(ushort lower, ushort upper)
+            public UShortRangeEnumerator(ushort lower, ushort upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = (ushort)(upper - (excluding ? 1u : 0u));
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -167,10 +169,10 @@ namespace SLThree
             public sbyte Lower;
             public sbyte Upper;
 
-            public SByteRangeEnumerator(sbyte lower, sbyte upper)
+            public SByteRangeEnumerator(sbyte lower, sbyte upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = (sbyte)(upper - (excluding ? 1 : 0));
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -187,10 +189,10 @@ namespace SLThree
             public byte Lower;
             public byte Upper;
 
-            public ByteRangeEnumerator(byte lower, byte upper)
+            public ByteRangeEnumerator(byte lower, byte upper, bool excluding = false)
             {
                 Lower = lower;
-                Upper = upper;
+                Upper = (byte)(upper - (excluding ? 1 : 0));
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -212,7 +214,7 @@ namespace SLThree
                 {
                     var lower = LowerBound.GetValue(context).Cast<long>();
                     var upper = UpperBound.GetValue(context).Cast<long>();
-                    return new RangeEnumerator(lower, upper);
+                    return new RangeEnumerator(lower, upper, Excluding);
                 }
                 else
                 {
@@ -220,35 +222,35 @@ namespace SLThree
                     if (type == typeof(long))
                         return new LongRangeEnumerator(
                             LowerBound.GetValue(context).Cast<long>(),
-                            UpperBound.GetValue(context).Cast<long>());
+                            UpperBound.GetValue(context).Cast<long>(), Excluding);
                     if (type == typeof(ulong))
                         return new ULongRangeEnumerator(
                             LowerBound.GetValue(context).Cast<ulong>(),
-                            UpperBound.GetValue(context).Cast<ulong>());
+                            UpperBound.GetValue(context).Cast<ulong>(), Excluding);
                     if (type == typeof(int))
                         return new IntRangeEnumerator(
                             LowerBound.GetValue(context).Cast<int>(),
-                            UpperBound.GetValue(context).Cast<int>());
+                            UpperBound.GetValue(context).Cast<int>(), Excluding);
                     if (type == typeof(uint))
                         return new UIntRangeEnumerator(
                             LowerBound.GetValue(context).Cast<uint>(),
-                            UpperBound.GetValue(context).Cast<uint>());
+                            UpperBound.GetValue(context).Cast<uint>(), Excluding);
                     if (type == typeof(short))
                         return new ShortRangeEnumerator(
                             LowerBound.GetValue(context).Cast<short>(),
-                            UpperBound.GetValue(context).Cast<short>());
+                            UpperBound.GetValue(context).Cast<short>(), Excluding);
                     if (type == typeof(ushort))
                         return new UShortRangeEnumerator(
                             LowerBound.GetValue(context).Cast<ushort>(),
-                            UpperBound.GetValue(context).Cast<ushort>());
+                            UpperBound.GetValue(context).Cast<ushort>(), Excluding);
                     if (type == typeof(sbyte))
                         return new SByteRangeEnumerator(
                             LowerBound.GetValue(context).Cast<sbyte>(),
-                            UpperBound.GetValue(context).Cast<sbyte>());
+                            UpperBound.GetValue(context).Cast<sbyte>(), Excluding);
                     if (type == typeof(byte))
                         return new ByteRangeEnumerator(
                             LowerBound.GetValue(context).Cast<byte>(),
-                            UpperBound.GetValue(context).Cast<byte>());
+                            UpperBound.GetValue(context).Cast<byte>(), Excluding);
                     throw new RangeIncorrectType(type, SourceContext);
                 }
             }
@@ -258,7 +260,7 @@ namespace SLThree
                 {
                     var lower = LowerBound.GetValue(context).CastToType<long>();
                     var upper = UpperBound.GetValue(context).CastToType<long>();
-                    return new RangeEnumerator(lower, upper);
+                    return new RangeEnumerator(lower, upper, Excluding);
                 }
                 else
                 {
@@ -266,45 +268,45 @@ namespace SLThree
                     if (type == typeof(long))
                         return new LongRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<long>(),
-                            UpperBound.GetValue(context).CastToType<long>());
+                            UpperBound.GetValue(context).CastToType<long>(), Excluding);
                     if (type == typeof(ulong))
                         return new ULongRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<ulong>(),
-                            UpperBound.GetValue(context).CastToType<ulong>());
+                            UpperBound.GetValue(context).CastToType<ulong>(), Excluding);
                     if (type == typeof(int))
                         return new IntRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<int>(),
-                            UpperBound.GetValue(context).CastToType<int>());
+                            UpperBound.GetValue(context).CastToType<int>(), Excluding);
                     if (type == typeof(uint))
                         return new UIntRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<uint>(),
-                            UpperBound.GetValue(context).CastToType<uint>());
+                            UpperBound.GetValue(context).CastToType<uint>(), Excluding);
                     if (type == typeof(short))
                         return new ShortRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<short>(),
-                            UpperBound.GetValue(context).CastToType<short>());
+                            UpperBound.GetValue(context).CastToType<short>(), Excluding);
                     if (type == typeof(ushort))
                         return new UShortRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<ushort>(),
-                            UpperBound.GetValue(context).CastToType<ushort>());
+                            UpperBound.GetValue(context).CastToType<ushort>(), Excluding);
                     if (type == typeof(sbyte))
                         return new SByteRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<sbyte>(),
-                            UpperBound.GetValue(context).CastToType<sbyte>());
+                            UpperBound.GetValue(context).CastToType<sbyte>(), Excluding);
                     if (type == typeof(byte))
                         return new ByteRangeEnumerator(
                             LowerBound.GetValue(context).CastToType<byte>(),
-                            UpperBound.GetValue(context).CastToType<byte>());
+                            UpperBound.GetValue(context).CastToType<byte>(), Excluding);
                     throw new RangeIncorrectType(type, SourceContext);
                 }
             }
         }
 
-        public override string ExpressionToString() => $"{(RangeType == null ? "" : $"<{RangeType}>")}{LowerBound}..{UpperBound}";
+        public override string ExpressionToString() => $"{(RangeType == null ? "" : $"<{RangeType}>")}{LowerBound}..{(Excluding?"=":"")}{UpperBound}";
 
         public override object Clone()
         {
-            return new CreatorRange(LowerBound.CloneCast(), UpperBound.CloneCast(), RangeType.CloneCast(), SourceContext.CloneCast());
+            return new CreatorRange(LowerBound.CloneCast(), UpperBound.CloneCast(), RangeType.CloneCast(), Excluding, SourceContext.CloneCast());
         }
     }
 }
