0a2d7476dee3b7cb72054e8e78fabd2ec0de670a
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-05-14 22:09:32 +0300

    TestSuite.Extensions

---
+++ TestSuite/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using SLThree;
+using SLThree.Extensions;
+using System.Linq;
+using System.Text;
+
+namespace TestSuite
+{
+    public class Extensions
+    {
+        public static ExecutionContext GetOptionsFromStatements(BaseStatement statement)
+        {
+            if (statement is StatementList sl)
+            {
+                var optExpr = sl.Statements.FirstOrDefault(x => x is ExpressionStatement est && est.Expression is FunctionDefinition fd && fd.FunctionName is NameExpression fn && fn.Name == "options");
+
+                if (optExpr == null) return null;
+
+                var optFunc = ((optExpr as ExpressionStatement).Expression as FunctionDefinition);
+
+                return ((optFunc?.GetValue(new ExecutionContext()) as Method)?.GetValue(new object[0]) as ContextWrap).Context;
+            }
+            return null;
+        }
+
+        public static (bool, string) GetDiff(string[] left, string[] right)
+        {
+            var sb = new StringBuilder();
+
+            var table_height = left.Length + right.Length;
+
+            var carriage_left = 0;
+            var carriage_right = 0;
+
+            var ret = true;
+
+            while (carriage_left < left.Length && carriage_right < right.Length)
+            {
+                if (left[carriage_left] != right[carriage_right])
+                {
+                    sb.AppendLine($"{carriage_left + 1,4}: {left[carriage_left].Length,4}/{right[carriage_right].Length,4}");
+                    ret = false;
+                }
+                carriage_left += 1; carriage_right += 1;
+            }
+            sb.AppendLine("=====OUTPUT\n" + right.JoinIntoString("\n") + "\n=====END OUTPUT");
+
+            return (ret, sb.ToString());
+        }
+    }
+}
--- TestSuite/Program.cs
+++ TestSuite/Program.cs
@@ -159,46 +159,6 @@ namespace TestSuite
             }
         }
 
-        public static ExecutionContext GetOptionsFromStatements(BaseStatement statement)
-        {
-            if (statement is StatementList sl)
-            {
-                var optExpr = sl.Statements.FirstOrDefault(x => x is ExpressionStatement est && est.Expression is FunctionDefinition fd && fd.FunctionName is NameExpression fn && fn.Name == "options");
-
-                if (optExpr == null) return null;
-
-                var optFunc = ((optExpr as ExpressionStatement).Expression as FunctionDefinition);
-
-                return ((optFunc?.GetValue(new ExecutionContext()) as Method)?.GetValue(new object[0]) as ContextWrap).Context;
-            }
-            return null;
-        }
-
-        public static (bool, string) GetDiff(string[] left, string[] right)
-        {
-            var sb = new StringBuilder();
-
-            var table_height = left.Length + right.Length;
-
-            var carriage_left = 0;
-            var carriage_right = 0;
-
-            var ret = true;
-
-            while (carriage_left < left.Length && carriage_right < right.Length)
-            {
-                if (left[carriage_left] != right[carriage_right])
-                {
-                    sb.AppendLine($"{carriage_left + 1,4}: {left[carriage_left].Length,4}/{right[carriage_right].Length,4}");
-                    ret = false;
-                }
-                carriage_left += 1; carriage_right += 1;
-            }
-            sb.AppendLine("=====OUTPUT\n" + right.JoinIntoString("\n") + "\n=====END OUTPUT");
-
-            return (ret, sb.ToString());
-        }
-
         public static bool RestoreTest(string filename)
         {
             try
@@ -206,11 +166,11 @@ namespace TestSuite
                 var input_lines = File.ReadAllLines(filename);
                 var input_code = input_lines.JoinIntoString("\n");
                 var parsed = Parser.This.ParseScript(input_code, filename);
-                var options = GetOptionsFromStatements(parsed);
+                var options = Extensions.GetOptionsFromStatements(parsed);
                 var restored_code = DefaultRestorator.GetRestorator<Restorator>().Restore(parsed, options);
                 var restored_lines = restored_code.Replace("\r", "").Split('\n');
 
-                var (result, errorLog) = GetDiff(input_lines, restored_lines);
+                var (result, errorLog) = Extensions.GetDiff(input_lines, restored_lines);
                 if (!result)
                 {
                     ErrorLog.Add($"!!!DIFFERENCE in {filename}:\n{errorLog}\n");
