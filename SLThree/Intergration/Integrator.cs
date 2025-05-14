2fc681a499046eee2f52e099a0471d32b3b77e96
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-05-15 00:14:25 +0300

    new Integrator
    + TestSuite project version update

--- SLThree/Intergration/Integrator.cs
+++ SLThree/Intergration/Integrator.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SLThree.Extensions;
+using SLThree.Metadata;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -7,14 +10,11 @@ namespace SLThree.Intergration
 {
     public class Integrator
     {
-        public interface ILogger
-        {
-            void Log(int level, string message);
-        }
         internal interface ICompilationUnit
         {
-            BaseStatement Compile();
+            BaseStatement Compile(IParser parser);
         }
+
         internal class FileUnit : ICompilationUnit
         {
             public string FileName { get; set; }
@@ -24,9 +24,9 @@ namespace SLThree.Intergration
                 FileName = fileName;
             }
 
-            public BaseStatement Compile()
+            public BaseStatement Compile(IParser parser)
             {
-                throw new NotImplementedException();
+                return parser.ParseScript(File.ReadAllText(FileName), FileName);
             }
         }
         internal class ResourceUnit : ICompilationUnit
@@ -40,60 +40,210 @@ namespace SLThree.Intergration
                 ResourceName = resourceName;
             }
 
-            public BaseStatement Compile()
+            public BaseStatement Compile(IParser parser)
             {
-                throw new NotImplementedException();
+                return parser.ParseScript(Assembly.GetManifestResourceStream(ResourceName).ReadString(), ResourceName);
+            }
+        }
+        internal class CodeUnit : ICompilationUnit
+        {
+            public string Code { get; set; }
+            public string ScriptName { get; set; }
+            public CodeUnit(string code, string scriptName)
+            {
+                Code = code;
+                ScriptName = scriptName;
+            }
+            public BaseStatement Compile(IParser parser)
+            {
+                return parser.ParseScript(Code, ScriptName ?? "script");
+            }
+        }
+        internal class AlreadyCompiledUnit : ICompilationUnit
+        {
+            public BaseStatement Compiled { get; set; }
+
+            public AlreadyCompiledUnit(BaseStatement compiled)
+            {
+                Compiled = compiled;
+            }
+
+            public BaseStatement Compile(IParser parser)
+            {
+                return Compiled;
+            }
+        }
+        internal class NestedScriptBuilderUnit : ICompilationUnit
+        {
+            public ScriptBuilder ScriptBuilder { get; set; }
+            public IParser Parser { get; set; }
+
+            public NestedScriptBuilderUnit(ScriptBuilder scriptBuilder, IParser parser)
+            {
+                ScriptBuilder = scriptBuilder;
+                Parser = parser;
+            }
+
+            public BaseStatement Compile(IParser parser)
+            {
+                var stst = ScriptBuilder
+                    .Compile(Parser ?? parser)
+                    .CompiledStatements;
+
+                if (stst.Length < 1)
+                    return new EmptyStatement(new SourceContext());
+
+                var ctx = stst[0].SourceContext;
+
+                return new StatementList(stst, ctx);
             }
         }
 
-        public class EnvironmentBuilder
+        public class RunOptions
         {
-            private ILogger _logger;
-            public EnvironmentBuilder(ILogger logger)
+            public bool AlwaysPrepareContext { get; set; } = true;
+        }
+        public class BuilderOptions
+        {
+            public bool ThrowIfError { get; set; } = true;
+            public bool UseLogging { get; set; } = false;
+            public Action<string> Logger { get; set; } = x => Console.WriteLine(x);
+            public Action<Exception> ExceptionHandler { get; set; }
+
+            public BuilderOptions()
             {
-                _logger = logger;
-                _logger = _logger ?? throw new ArgumentNullException(nameof(logger));
+                ExceptionHandler = x =>
+                {
+                    if (UseLogging)
+                        Logger(x.ToString());
+                };
             }
+        }
+
+        public class ScriptBuilder
+        {
+            public BuilderOptions Options { get; set; }
+            public bool Success { get; set; } = false;
+            public bool Compiled => CompiledStatements != null;
+            public BaseStatement[] CompiledStatements { get; set; } = null;
+
+            public ScriptBuilder(Action<BuilderOptions> config)
+            {
+                Options = new BuilderOptions();
+                config(Options);
+                units = new List<ICompilationUnit>();
+            }
+            public ScriptBuilder() : this(cfg => { }) { }
 
             private List<ICompilationUnit> units;
 
-            public EnvironmentBuilder WithFile(string fileName)
+            public ScriptBuilder WithCode(string code, string scriptname = null)
+            {
+                units.Add(new CodeUnit(code, scriptname ?? Guid.NewGuid().ToString()));
+                return this;
+            }
+            public ScriptBuilder WithFile(string fileName)
             {
                 units.Add(new FileUnit(fileName));
                 return this;
             }
-            public EnvironmentBuilder WithFile(params string[] fileNames)
+            public ScriptBuilder WithFiles(params string[] fileNames)
             {
                 foreach (var fileName in fileNames)
                     WithFile(fileName);
                 return this;
             }
-            public EnvironmentBuilder WithResource(string resourceName)
+
+            public ScriptBuilder WithDirectory(string directory, bool only_slt = true, bool onlytop = false)
+            {
+                var fileNames = Directory.GetFiles(directory, only_slt ? "*.slt" : "*", onlytop ? SearchOption.TopDirectoryOnly : SearchOption.AllDirectories);
+                foreach (var fileName in fileNames)
+                    WithFile(fileName);
+                return this;
+            }
+
+            public ScriptBuilder WithDirectories(string[] directories, bool only_slt = true, bool onlytop = false)
+            {
+                foreach (var directoryName in directories)
+                    WithDirectory(directoryName, only_slt, onlytop);
+                return this;
+            }
+
+            public ScriptBuilder WithDirectories(params string[] directories)
+            {
+                foreach (var directoryName in directories)
+                    WithDirectory(directoryName);
+                return this;
+            }
+
+            public ScriptBuilder WithResource(string resourceName)
             {
                 units.Add(new ResourceUnit(Assembly.GetEntryAssembly(), resourceName));
                 return this;
             }
-            public EnvironmentBuilder WithResources(params string[] resourceNames)
+            public ScriptBuilder WithResources(params string[] resourceNames)
             {
                 foreach (var resourceName in resourceNames)
                     WithResources(resourceName);
                 return this;
             }
-            public EnvironmentBuilder WithResource(Assembly assembly, string resourceName)
+            public ScriptBuilder WithResource(Assembly assembly, string resourceName)
             {
                 units.Add(new ResourceUnit(assembly, resourceName));
                 return this;
             }
-            public EnvironmentBuilder WithResources(Assembly assembly, params string[] resourceNames)
+            public ScriptBuilder WithResources(Assembly assembly, params string[] resourceNames)
             {
                 foreach (var resourceName in resourceNames)
                     WithResources(assembly, resourceName);
                 return this;
             }
-
-            public void Build()
+            
+            public ScriptBuilder AddBuilder(ScriptBuilder scriptBuilder, IParser parser = null)
             {
-                var statements = units.Select(x => x.Compile()).ToArray();
+                units.Add(new NestedScriptBuilderUnit(scriptBuilder, parser));
+                return this;
+            }
+            public ScriptBuilder AddCompiled(BaseStatement statement)
+            {
+                units.Add(new AlreadyCompiledUnit(statement));
+                return this;
+            }
+
+            public ScriptBuilder Compile(IParser parser)
+            {
+                var statements = new List<BaseStatement>();
+                Success = true;
+                foreach (var unit in units)
+                {
+                    try
+                    {
+                        statements.Add(unit.Compile(parser));
+                    }
+                    catch (Exception ex)
+                    {
+                        Options.ExceptionHandler(ex);
+                        Success = false;
+                        if (Options.ThrowIfError)
+                            throw;
+                    }
+                }
+                CompiledStatements = statements.ToArray();
+                return this;
+            }
+
+            public ExecutionContext Run(RunOptions options = null, ExecutionContext context = null)
+            {
+                options = options ?? new RunOptions();
+                context = context ?? new ExecutionContext();
+
+                foreach (var compiled in CompiledStatements)
+                {
+                    if (options.AlwaysPrepareContext)
+                        context.PrepareToInvoke();
+                    compiled.GetValue(context);
+                }
+                return context;
             }
         }
     }
---
+++ SampleProjects/IntegratorWay/Program.cs
@@ -0,0 +1,30 @@
+﻿using SLThree.Intergration;
+using SLThree.Language;
+
+namespace IntegratorWay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var envBuilder = new Integrator.ScriptBuilder();
+
+            var nestedBuilder = new Integrator.ScriptBuilder()
+                .WithFile("output.slt")
+                .WithResource("IntegratorWay.resource.slt");
+
+            var ret = envBuilder
+                .WithCode("x = 2; return x + x * x;")
+                .WithCode("y = self.unwrap().ReturnedValue; return y * 1000;")
+                .AddBuilder(nestedBuilder)
+                .Compile(new Parser())
+                .Run();
+
+
+            Console.WriteLine(ret.ReturnedValue);
+            Console.WriteLine("output: " + (ret.LocalVariables.GetValue("output").Item1?.ToString() ?? "null"));
+            Console.WriteLine("resource: " + (ret.LocalVariables.GetValue("resource").Item1?.ToString() ?? "null"));
+            Console.ReadLine();
+        }
+    }
+}
