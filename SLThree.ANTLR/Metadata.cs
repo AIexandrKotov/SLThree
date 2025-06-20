429ce7b7ac8aa30c2f901ae0d3da871358f5a609
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-06-21 00:38:55 +0300

    antlr start

---
+++ SLThree.ANTLR/Metadata.cs
@@ -0,0 +1,19 @@
+﻿using SLThree.Extensions;
+using SLThree.Language;
+using SLThree.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SLThree.ANTLR
+{
+    public class Metadata : IPluginInfo, ILanguageProvider, IApiPlugin
+    {
+        public string Name => "SLThree ANTLR";
+        public bool Insert => true;
+        public string Edition => "";
+        public IParser Parser => new Parser();
+        public IRestorator Restorator => new Restorator();
+        public KeyValuePair<string, Type>[] NewTypes => SLTHelpers.GetTypesFromAssemblySYS(typeof(Metadata).Assembly, "SLThree.Pascal.sys.").ToArray();
+    }
+}
---
+++ SLThree.ANTLR/Parser.cs
@@ -0,0 +1,18 @@
+﻿using SLThree.Metadata;
+using System;
+
+namespace SLThree.ANTLR
+{
+    public class Parser : IParser
+    {
+        public BaseExpression ParseExpression(string code, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseStatement ParseScript(string code, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
