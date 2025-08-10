a2de59e86d9eb06e25ec5ac162966dbce3b83311
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-08-10 18:33:32 +0300

    Create di.cs

---
+++ SLThree/sys/di.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SLThree.sys
+{
+#pragma warning disable IDE1006 // Стили именования
+    public static class di
+    {
+        public static IServiceProvider GetProvider() => ExecutionContext.global.Context.@private["%DI"] as IServiceProvider;
+
+        public static void RegisterSLThreeServiceProvider(this IServiceProvider serviceProvider)
+        {
+            ExecutionContext.global.Context.@private["%DI%"] = serviceProvider;
+        }
+
+        public static object GetService<T>()
+        {
+            return GetProvider().GetService(typeof(T));
+        }
+
+        public static object GetService(Type type)
+        {
+            return GetProvider().GetService(type);
+        }
+    }
+#pragma warning restore IDE1006 // Стили именования
+}
